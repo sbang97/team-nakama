234b27d135ab3eecb9857e1f84b596009db00f5f
Simon Bang <bangtsimon@gmail.com>
2019-03-13 15:53:52 -0700

    removed test divs

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,18 +7,6 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div className={classnames("App", this.props.className)}>
-				<div className="project">
-					Movie Shopper
-				</div>
-				<div className="project">
-					Web Crawler
-				</div>
-				<div className="project">
-					Bibimbap King
-				</div>
-				<div className="project">
-					Project #4
-				</div>
 			</div>
 		);
 	}
