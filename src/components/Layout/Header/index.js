7f61ee652a9374f7011649f568c9388558fda08a
Simon Bang <bangtsimon@gmail.com>
2019-03-13 15:54:08 -0700

    added basic navigation

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -3,23 +3,23 @@ import classnames from "classnames";
 import logo from "../logo.svg";
 import { IndexLink } from "react-router";
 import NavLink from "../NavLink";
+import Nav from 'react-bootstrap/Nav';
 import "./style.css";
 
 export default class Header extends Component {
 	render() {
-		return(
-			<header className={classnames("Header", this.props.className)}>
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Youth Education on Mental Illness</h2>
-				</div>
-				<nav>
-					<ul>
-						<li><IndexLink to="/" activeClassName="active">home</IndexLink></li>
-						<li><NavLink to="/about">about</NavLink></li>
-					</ul>
-				</nav>
-			</header>
+		return (
+			<Nav className="justify-content-end" activeKey="/home">
+				<Nav.Item>
+					<Nav.Link href="/">Home</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link href="/about">About</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link eventKey="/resources">Learn More</Nav.Link>
+				</Nav.Item>
+			</Nav>
 		);
 	}
 }
