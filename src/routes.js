e95c003f16947750dba53352b3ee8e478cfc0be1
Simon Bang <bangtsimon@gmail.com>
2019-04-23 23:58:57 -0700

    test routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
 import Resources from "./components/Resources";
+import NotFound from "./components/NotFound";
 
 const Routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={Home} />
     <Route path="/about" component={About} />
     <Route path="/resources" component={Resources} />
+    <Route path="*" component={NotFound} />
   </Route>
 );
 
