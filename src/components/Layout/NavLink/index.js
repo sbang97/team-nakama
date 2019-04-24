8b57508fcc544b6712df61bd35677e8a905bbb6c
Simon Bang <bangtsimon@gmail.com>
2019-04-24 00:06:39 -0700

    Updates

--- src/components/Layout/NavLink/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { Link } from "react-router";
-import classnames from "classnames";
-
-export default class NavLink extends Component {
-	render() {
-		return <Link className={classnames("NavLink", this.props.className)} {...this.props} activeClassName="active"/>;
-	}
-}
